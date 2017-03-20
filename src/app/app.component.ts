d75404b1e8010ff2ac5a42b3773a9009a238bf0b
perttu <savolainen.perttu@gmail.com>
2017-03-20 15:45:58 +0200

    project created, added basic drag n drop with png and jpeg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  images:Array<Object> = [];
+
+  constructor() {}
+
+  handleDrop(e) {
+    let files:File = e.dataTransfer.files;
+    let self = this;
+    Object.keys(files).forEach((key) => {
+      if(files[key].type === "image/png" || files[key].type === "image/jpeg") {
+        self.images.push(files[key]);
+      }
+      else {
+        alert("File must be a PNG or JPEG!");
+      }
+    });
+
+    return false;
+  }
+
 }
